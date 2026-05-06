0fd0d50c44c370c6c0d4655d71df88857538f83f
X1nm1n <148549361+X1nm1n@users.noreply.github.com>
2026-05-06 09:47:31 +0200

    lab4

---
+++ app/data/db.py
@@ -0,0 +1,23 @@
+from sqlmodel import create_engine, SQLModel, Session
+from typing import Annotated
+from fastapi import Depends
+
+
+sqlite_file_name = "/home/asotgiu/lab_2026/app/data/database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+engine = create_engine(
+    sqlite_url,
+    connect_args={"check_same_thread": False},
+    echo=True
+)
+
+
+def init_database():
+    SQLModel.metadata.create_all(engine)
+
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+SessionDep = Annotated[Session, Depends(get_session)]
